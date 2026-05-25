85662080dd4ef33fba7d6c4646f81c5aacab4478
Noé Masurel <noe.masurel@polymtlus.ca>
2026-05-25 12:13:50 -0400

    Initial commit

---
+++ scrapping.py
@@ -0,0 +1,136 @@
+import os
+from dotenv import load_dotenv
+from playwright.sync_api import sync_playwright, Playwright
+import csv
+import pandas as pd
+
+historique = {}
+CSV_PATH = 'data/statistiques_trajets.csv'
+
+# Charge les variables du fichier .env dans l'environnement système
+load_dotenv()
+
+# Récupère les valeurs
+username = os.getenv('BIXI_USERNAME')
+password = os.getenv('BIXI_PASSWORD')
+
+
+if not username or not password:
+    raise ValueError("Identifiants manquants : vérifie ton fichier .env")
+
+print(f"Prêt à connecter l'utilisateur : {username}")
+
+
+def connect(browser):
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://secure.bixi.com/sign-in?next=http%3A%2F%2Fsecure.bixi.com%2Fprofile")
+    try:
+        page.wait_for_selector("#onetrust-accept-btn-handler", timeout=5000)
+        page.click("#onetrust-accept-btn-handler")
+        print("Bannière de cookies acceptée.")
+    except:
+        print("Pas de bannière de cookies détectée.")
+    page.fill('input[name="bssUsername"]', username)
+    page.fill('input[name="bssPassword"]', password)
+    page.click('span[data-testid="icon-button-text"]')
+    page.wait_for_timeout(1000)
+    return context, page
+
+
+def run(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    
+    # On vérifie si le fichier existe pour éviter de se reconnecter
+    if os.path.exists(CSV_PATH):
+        df_existing = pd.read_csv(CSV_PATH)
+        already_scraped = int(df_existing['nb'].sum())
+        print(f"Trajets déjà enregistrés dans le CSV : {already_scraped}")
+    else:
+        already_scraped = 0
+        print("Aucun fichier CSV existant, on part de zéro.")
+
+    if os.path.exists("data/state.json"):
+        context = browser.new_context(storage_state="data/state.json")
+        page = context.new_page()
+        page.goto("https://secure.bixi.com/profile")
+    else:
+        context, page = connect(browser)
+
+    context.storage_state(path="data/state.json")
+    total_text = page.locator(r"text=/\d+ rides taken./").inner_text()
+    total_trajets = int(total_text.split()[0])
+    print(f"Total BIXI : {total_trajets}")
+    page.get_by_role("link", name="Ride history").click()
+
+    nb_trajets = total_trajets - already_scraped
+    if nb_trajets == 0:
+        print("Aucun nouveau trajet.")
+        input("Appuie sur Entrée pour fermer le navigateur...")
+        browser.close()
+        return
+
+    for i in range(nb_trajets//10):
+        show_more_locator = page.locator('[data-testid="DATA_TESTID_SHOW_MORE"]') # pyright: ignore[reportPossiblyUnboundVariable]
+        show_more_locator.scroll_into_view_if_needed()
+        show_more_locator.click()
+
+        #page.wait_for_load_state("networkidle")
+        page.wait_for_timeout(250)
+
+
+    trajets = page.locator('[data-testid="DATA_TESTID_RIDE_OVERVIEW_CARD"]').count()
+    print(f"Nombre de trajets à ouvrir : {trajets}")
+    for i in range(nb_trajets):
+        trajet = page.locator('[data-testid="DATA_TESTID_RIDE_OVERVIEW_CARD"]').nth(i)
+        
+        trajet.scroll_into_view_if_needed()
+        trajet.click()
+        page.wait_for_timeout(250) 
+        try:
+            depart = page.locator('[class*="Start"]').locator('div').first.inner_text().split('\n')[0]
+            arrivee = page.locator('[class*="End"]').locator('div').first.inner_text().split('\n')[0]
+            print(f"Trajet {i+1} : {depart} -> {arrivee}")
+            itineraire = (depart.strip(), arrivee.strip())
+
+            if itineraire in historique:
+                historique[itineraire] += 1
+            else:
+                historique[itineraire] = 1
+            
+        except Exception as e:
+            print(f"Erreur d'extraction pour le trajet {i+1}")
+            input("Appuie sur Entrée pour fermer le navigateur...")
+        trajet.click()
+
+    path = "data/statistiques_trajets.csv"
+
+    if os.path.exists(path):
+        df_old = pd.read_csv(path)
+    else:
+        df_old = pd.DataFrame(columns=["depart", "arrivee", "nb"])
+
+    df_new = pd.DataFrame(
+        [(dep, arr, nb) for (dep, arr), nb in historique.items()],
+        columns=["depart", "arrivee", "nb"]
+    )
+
+    # 3. Fusion + agrégation
+    df = pd.concat([df_old, df_new], ignore_index=True)
+    df = df.groupby(["depart", "arrivee"], as_index=False)["nb"].sum()
+
+    # CHECKK SYNTAX
+    # df = df.sort_values(by=["nb", "depart", "arrivee"], ascending=(False, True, True))
+    df.to_csv(path, index=False)
+
+
+    input("Appuie sur Entrée pour fermer le navigateur...")
+    
+    browser.close()
+        
+def main():
+    with sync_playwright() as playwright:
+        run(playwright)
+        
+if __name__ == "__main__":
+    main()
---
+++ traces.py
@@ -0,0 +1,113 @@
+import os
+import pandas as pd
+import osmnx as ox
+import networkx as nx
+import folium
+# from folium.plugins import HeatMap
+import pickle
+
+STATS_CSV = "data/statistiques_trajets.csv"
+COORDS_CSV = "data/bixi_stations.csv"
+MAP_FILE = "data/montreal_bike.graphml"
+PLACE = "Montreal, Canada"
+CACHE_FILE = "data/route_cache.pkl"
+
+
+
+def get_graph():
+    if os.path.exists(MAP_FILE):
+        print(f"Chargement de la carte locale...")
+        return ox.load_graphml(MAP_FILE)
+    else:
+        print(f"Téléchargement de la carte (Montreal)...")
+        G = ox.graph_from_place(PLACE, network_type="bike")
+        ox.save_graphml(G, filepath=MAP_FILE)
+        return G
+
+def main():
+    try:
+        df_stats = pd.read_csv(STATS_CSV)
+    except FileNotFoundError:
+        print(f"Erreur : Le fichier {STATS_CSV} est introuvable.")
+        return
+    
+    try:
+        stations_df = pd.read_csv(COORDS_CSV)
+    except FileNotFoundError:
+        print(f"Erreur : Le fichier {COORDS_CSV} est introuvable.")
+        return
+
+    if os.path.exists(CACHE_FILE):
+        with open(CACHE_FILE, "rb") as f:
+            route_cache = pickle.load(f)
+        print(f"Loaded {len(route_cache)} cached routes")
+    else:
+        route_cache = {}
+
+
+    station_to_lat = dict(zip(stations_df["name"], stations_df["lat"]))
+    station_to_lon = dict(zip(stations_df["name"], stations_df["lon"]))
+
+    G = get_graph()
+    for u, v, k, data in G.edges(data=True, keys=True):
+        data['bike_weight'] = data['length']
+        # Si ce n'est pas une infrastructure cyclable connue
+        if not any(key in data for key in ['cycleway', 'bicycle', 'amenity']):
+            data['bike_weight'] *= 5 # On pénalise fortement les routes standards
+
+    m = folium.Map(location=[45.5088, -73.5878], zoom_start=13, tiles="cartodbpositron")
+
+    print(f"Calcul des trajets pour {len(df_stats)} lignes...")
+
+    heat_data = []
+    for index, row in df_stats.iterrows():
+        try:
+            key = (row["depart"], row["arrivee"])
+            if key in route_cache:
+                route_coords = route_cache[key]
+            #     heat_data.append([dep_lat, dep_lon, row["nb"]])
+            #     heat_data.append([arr_lat, arr_lon, row["nb"]])
+            else : 
+                dep_lat = station_to_lat.get(row["depart"],0)
+                dep_lon = station_to_lon.get(row["depart"],0)
+
+                arr_lat = station_to_lat.get(row["arrivee"],0)
+                arr_lon = station_to_lon.get(row["arrivee"],0)
+
+                # heat_data.append([dep_lat, dep_lon, row["nb"]])
+                # heat_data.append([arr_lat, arr_lon, row["nb"]])
+
+                dep = (dep_lat,dep_lon)
+                arr = (arr_lat, arr_lon)
+
+                # ox.nearest_nodes  (G, X=longitude, Y=latitude)
+                orig_node = ox.nearest_nodes(G, dep[1], dep[0])
+                dest_node = ox.nearest_nodes(G, arr[1], arr[0])
+
+                route = nx.shortest_path(G, orig_node, dest_node, weight="length")
+                
+                route_coords = [(G.nodes[n]['y'], G.nodes[n]['x']) for n in route]
+
+                route_cache[key] = route_coords
+                
+            folium.PolyLine(route_coords, 
+                            weight=row['nb']*3,
+                            color="blue",
+                            opacity=0.5
+                            ).add_to(m)
+
+        except Exception as e:
+            print(f"Erreur à la ligne {index}: {e}")
+            continue
+
+    #HeatMap(heat_data, radius=15, blur=10, max_zoom=13).add_to(m)       
+    # 5. Sauvegarde
+    with open(CACHE_FILE, "wb") as f:
+        pickle.dump(route_cache, f)
+        print(f"Saved {len(route_cache)} cached routes")
+
+    m.save("data/resultat_bixi_csv.html")
+    print("Visualisation générée dans : resultat_bixi_csv.html")
+
+if __name__ == "__main__":
+    main()
